73218c67d01a540f3b10f7966881c359d04f6714
Jacek Andrzejewski <jacaato@gmail.com>
2013-10-11 20:52:22 +0200

    initial commit for android client

--- client/AdminTools/src/pl/edu/agh/zpi/admintools/AdminTools.java
+++ client/AdminTools/src/pl/edu/agh/zpi/admintools/AdminTools.java
@@ -1,21 +1,21 @@
-package pl.zpi.admintools;
+package pl.edu.agh.zpi.admintools;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class AdminTools extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_admin_tools);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.admin_tools, menu);
 		return true;
 	}
 
