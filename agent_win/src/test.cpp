94e39bf97c4635bba80585285d985d2c3c1181ea
Jakub <elsetage@gmail.com>
2013-10-24 16:52:35 +0200

    Kilka zmian - rezygnacja z pdh.h
    Cpu
    TotalRAM
    numCores

--- agent_win/src/test.cpp
+++ agent_win/src/test.cpp
@@ -1,9 +1,15 @@
+#define _WIN32_WINNT  0x0501
+
 #include <cstdlib>
 #include <iostream>
 #include "windows.h"
 #include "Config.h"
 #include "Config.cpp"
 #include "sensors.h"
+#include "windows.h"
+#include "winbase.h"
+
+
 
 extern "C" {
 #include "sigar.h"
@@ -13,42 +19,60 @@ extern "C" {
 
 void readSensors(TSensorsData& data) {   
    
-   sigar_t *sigar_cpu;
-   sigar_cpu_t old;
-   sigar_cpu_t current;
+    sigar_t *sigar_cpu;
+    sigar_cpu_t old;
+    sigar_cpu_t current;
 
-   // Ustawienie CpuUsage
+    // Ustawienie CpuUsage
    
-   sigar_open(&sigar_cpu);
-   sigar_cpu_get(sigar_cpu, &old);
+    sigar_open(&sigar_cpu);
+    sigar_cpu_get(sigar_cpu, &old);
    
-   sigar_cpu_perc_t perc;
-   double avg = 0.0;
-
-   for (int i = 0; i < 5; i++) {
-        sigar_cpu_get(sigar_cpu, &current);
-        sigar_cpu_perc_calculate(&old, &current, &perc);
-        cout << perc.combined * 100 << std::endl;
-        
-        if (i != 0)
-           avg = avg + perc.combined * 100;
-        
-        old = current;
-        Sleep(100);
-    }
-    avg = avg/4;
+    sigar_cpu_perc_t perc;
+    double avg = 0.0;
+    Sleep(100);
+   
+    sigar_cpu_get(sigar_cpu, &current);
+    sigar_cpu_perc_calculate(&old, &current, &perc);
+    avg = perc.combined * 100;
+   
     cout << "Cpu Usage: " << avg << std::endl;
     data.cpuUsage = avg;
     
     sigar_close(sigar_cpu);
+    
+    // Liczba rdzeni
+    
+    SYSTEM_INFO sysinfo;
+    GetSystemInfo( &sysinfo );
+    
+    int numCPU = sysinfo.dwNumberOfProcessors;
+    
+    cout << "Number of cores: " << numCPU << std::endl;
 	
     // Ustawienie temperatury
+    
+    
+    // Pamiêæ
+    
+    MEMORYSTATUSEX statex;
+
+    statex.dwLength = sizeof (statex); 
+
+    GlobalMemoryStatusEx (&statex);
+    cout << "Physical RAM: " << (float)statex.ullTotalPhys/(1024*1024*1024)<< endl;
+     
+    data.totalRam = (float)statex.ullTotalPhys/(1024*1024*1024);
+
+    
+    
+    
+    
     
     data.tempValid = true;
     
     data.uptime = 0;
 	data.freeRam = 0;
-	data.totalRam = 0;
 	
 //	TDiskUsage d;
 //	d.name = dev;
@@ -67,7 +91,7 @@ int main() {
        return 0;
     };
 
-	printf ("%d\r\n", c.getInt ("port"));
+	printf ("Port: %d\r\n", c.getInt ("port"));
 
     TSensorsData t;
  
