8f4a66af1dac762a8c40d1dfb5ca1f9a5a3be5fe
Quepas <oneinchman.dev@gmail.com>
2013-11-14 22:07:55 +0100

    Adding Doxygen commentaries part 7 (agent-server, Android POJO).

--- client/AdminTools/src/pl/edu/agh/zpi/admintools/sensors/AgentData.java
+++ client/AdminTools/src/pl/edu/agh/zpi/admintools/sensors/AgentData.java
@@ -3,17 +3,32 @@ package pl.edu.agh.zpi.admintools.sensors;
 import java.io.Serializable;
 import java.nio.ByteBuffer;
 
+/**
+ *	\class AgentData
+ *	\brief Agent data structure as serializable POJO.
+ */
 public class AgentData implements Serializable{
+	//! Serialization id.
 	private static final long serialVersionUID = 1L;
+	//! Agent id.
 	private short id;
+	//! Old data.
 	@SuppressWarnings("unused")
 	private boolean oldData;
+	//! Agent name.
 	private String name = "";
+	//! Current sensor data from agent.
 	private SensorsData data;
 
 	private AgentData() {
 	}
 
+	/**
+	 *	\fn public static AgentData fromByteBuffer(ByteBuffer buffer)
+	 *	\brief Fill agent data with given byte buffer.
+	 *	\param buffer Byte buffer with agent data.
+	 *	\return Converted AgentData object.
+	 */
 	public static AgentData fromByteBuffer(ByteBuffer buffer) {
 		AgentData agent = new AgentData();
 		agent.id = buffer.getShort();
@@ -26,23 +41,49 @@ public class AgentData implements Serializable{
 		return agent;
 	}
 
+	/**
+	 *	\fn public short getId()
+	 *	\brief Get agent id.
+	 *	\return Agent id.
+	 */
 	public short getId(){
 		return id;
 	}
-	
+
+	/**
+	 *	\fn public SensorsData getData()
+	 *	\brief Get current sensor data from agent.
+	 *	\return Current sensor data.
+	 */
 	public SensorsData getData(){
 		return data;
 	}
-	
+
+	/**
+	 *	\fn public String getName()
+	 *	\brief Get agent name.
+	 *	\return Agent name.
+	 */
 	public String getName() {
 		return name;
 	}
-	
+
+	/**
+	 *	\fn public String toString()
+	 *	\brief Convert AgentData to string representation.
+	 *	\return String representation.
+	 */
 	@Override
 	public String toString() {
 		return "AgentData [id=" + id + ", data=" + data + "]";
 	}
-	
+
+	/**
+	 *	\fn public boolean equals(Object o)
+	 *	\brief Check equality of object with given parameter.
+	 *	\param o Object to check.
+	 *	\return If equlas.
+	 */
 	@Override
 	public boolean equals(Object o) {
 		if(o != null && !(o instanceof AgentData))
--- client/AdminTools/src/pl/edu/agh/zpi/admintools/sensors/DiskUsageData.java
+++ client/AdminTools/src/pl/edu/agh/zpi/admintools/sensors/DiskUsageData.java
@@ -3,15 +3,29 @@ package pl.edu.agh.zpi.admintools.sensors;
 import java.io.Serializable;
 import java.nio.ByteBuffer;
 
+/**
+ *	\class DiskUsageData
+ *	\brief Disc usage data structure as serializable POJO.
+ */
 public class DiskUsageData implements Serializable {
+	//! Serialization id.
 	private static final long serialVersionUID = 1L;
+	//! Disc name.
 	private String name = "";
+	//! Total available disc space.
 	private long totalSpace;
+	//! Used space on disc.
 	private long usedSpace;
 
 	private DiskUsageData() {
 	}
 
+	/**
+	 *	\fn public static DiskUsageData fromByteBuffer(ByteBuffer buffer)
+	 *	\brief Fill disc usage data with given byte buffer.
+	 *	\param buffer Byte buffer with disc usage.
+	 *	\return Converted DiskUsageData object.
+	 */
 	public static DiskUsageData fromByteBuffer(ByteBuffer buffer) {
 		DiskUsageData dud = new DiskUsageData();
 		for (short i = buffer.getShort(); i > 0; i--) {
@@ -23,18 +37,38 @@ public class DiskUsageData implements Serializable {
 		return dud;
 	}
 
+	/**
+	 *	\fn public String getName()
+	 *	\brief Get disc name.
+	 *	\return Disc name.
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 *	\fn public long getTotalSpace()
+	 *	\brief Get total available space on disc.
+	 *	\return Total space.
+	 */
 	public long getTotalSpace() {
 		return totalSpace;
 	}
 
+	/**
+	 *	\fn public long getUsedSpace()
+	 *	\brief Get used space on disc.
+	 *	\return Used space.
+	 */
 	public long getUsedSpace() {
 		return usedSpace;
 	}
 
+	/**
+	 *	\fn public String toString()
+	 *	\brief Convert DiskUsageData to string representation.
+	 *	\return String representation.
+	 */
 	@Override
 	public String toString() {
 		return "DiskUsageData [name=" + name + ", totalSpace=" + totalSpace
