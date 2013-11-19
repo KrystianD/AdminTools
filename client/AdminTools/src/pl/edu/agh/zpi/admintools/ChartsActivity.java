2f774535fdcfe6c95bfa6f6e1f80963a7c6042e0
Jacek Andrzejewski <jacaato@gmail.com>
2013-11-19 16:13:10 +0100

    poprawki

--- client/AdminTools/src/pl/edu/agh/zpi/admintools/ChartsActivity.java
+++ client/AdminTools/src/pl/edu/agh/zpi/admintools/ChartsActivity.java
@@ -105,6 +105,8 @@ public class ChartsActivity extends Activity implements ServiceConnection,
 		Calendar c = Calendar.getInstance();
 		c.set(Calendar.MINUTE, 0);
 		c.set(Calendar.HOUR_OF_DAY, 0);
+		c.set(Calendar.SECOND, 0);
+		c.set(Calendar.MILLISECOND, 0);
 		statsRequest.setStartDate((int) (c.getTime().getTime() / 1000));
 		Log.d("qwe", "" + (int) (c.getTime().getTime() / 1000));
 		today += c.get(Calendar.DAY_OF_MONTH) + "-";
