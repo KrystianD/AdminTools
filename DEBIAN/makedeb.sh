3b16adc8fd138a8dae9c0730eb70c27a2f7e6167
Krystian Dużyński <krystekd@gmail.com>
2013-11-19 22:06:58 +0100

    Poprawka generatora paczek

--- DEBIAN/makedeb.sh
+++ DEBIAN/makedeb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp server/build/server server-1.0/usr/local/zpiserver/server
-cp agent_lin/build/client agent-lin-1.0/usr/local/zpiclient/agent_lin
+cp ../server/build/server server-1.0/usr/local/zpiserver/server
+cp ../agent_lin/build/client agent-lin-1.0/usr/local/zpiclient/agent_lin
 dpkg-deb --build server-1.0
 dpkg-deb --build agent-lin-1.0
