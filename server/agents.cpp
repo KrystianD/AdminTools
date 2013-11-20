7b70869f2b8fef182923f11e4c8bb086ff0f4fb6
Krystian Dużyński <krystekd@gmail.com>
2013-11-20 22:32:15 +0100

    Sortowanie agentów dla agenta mobilnego

--- server/agents.cpp
+++ server/agents.cpp
@@ -1,6 +1,13 @@
 #include "agents.h"
 
+#include <algorithm>
+
 vector<TAgentData> agentsData;
+
+bool cmp (const TAgentData& a1, const TAgentData& a2)
+{
+	return a1.packet.name < a2.packet.name;
+}
 
 void assignData (TPacketAgentData& packet)
 {
@@ -17,4 +24,5 @@ void assignData (TPacketAgentData& packet)
 	data.time = time (0);
 	data.packet = packet;
 	agentsData.push_back (data);
+	sort (agentsData.begin (), agentsData.end (), cmp);
 }
